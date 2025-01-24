a74094a48a799473fe628cc0fbb1ce1c890a06ce
diez04 <u.deniz2424@gmail.com>
2025-01-24 13:44:38 +0100

    modify main.py to print out a book report.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def number_of_words(words):
     for word in words_lst:
         count += 1
 
-    print(count)
+    return count
 
 def count_characters(text):
     lowercase_text = text.lower()
@@ -27,5 +27,18 @@ def count_characters(text):
     return character_count
 
 
+def report(word_count, character_count):
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document\n")
+
+    for char in character_count:
+        if char.isalpha():
+            print(f"The '{char}' character was found {character_count[char]} times")
+
+    print("--- End report ---")
+
 book = main()
-print(count_characters(book))
+word_number = number_of_words(book)
+character_num = count_characters(book)
+report(word_number, character_num)
